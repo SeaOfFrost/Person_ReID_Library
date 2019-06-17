79ffc2be3c4ef4599b4b0410cbf76ccfe3abf9c1
Siddharth Sudhakaran <siddharth97@gmail.com>
2019-06-17 14:25:07 +0800

    Update MGN.py

--- models/MGN.py
+++ models/MGN.py
@@ -5,27 +5,13 @@ from torchvision import models
 from torchvision.models.resnet import Bottleneck
 from .BasicModule import BasicModule
 from .backbones.resnet import resnet50
-
-# def _init_reduction(reduction):
-#     # conv
-#     nn.init.kaiming_normal_(reduction[0].weight, mode='fan_in')
-#     # nn.init.constant_(reduction[0].bias, 0.)
-
-#     # bn
-#     nn.init.normal_(reduction[1].weight, mean=1., std=0.02)
-#     nn.init.constant_(reduction[1].bias, 0.)
-
-# def _init_fc(fc):
-#     nn.init.kaiming_normal_(fc.weight, mode='fan_out')
-#     # nn.init.normal_(fc.weight, std=0.001)
-#     nn.init.constant_(fc.bias, 0.)
         
-class MGN2(BasicModule):
-    feats = 256
-    n_feats = 2048
-    def __init__(self, num_classes, last_stride):
-        super(MGN2, self).__init__()
-        self.model_name = 'MGN2'
+class MGN_Paper(BasicModule):
+    feats_256 = 256
+    feats_2048 = 2048
+    def __init__(self, num_classes, last_stride, pool):
+        super(MGN_Paper, self).__init__()
+        self.model_name = 'MGN_Paper'
         self.download = models.resnet50(pretrained=True)
         self.base = resnet50(pretrained=True, last_stride=last_stride)
         
@@ -52,27 +38,34 @@ class MGN2(BasicModule):
         self.p1 = nn.Sequential(copy.deepcopy(res_conv4), copy.deepcopy(res_g_conv5))
         self.p2 = nn.Sequential(copy.deepcopy(res_conv4), copy.deepcopy(res_p_conv5))
         self.p3 = nn.Sequential(copy.deepcopy(res_conv4), copy.deepcopy(res_p_conv5))
+        
+        if pool == 'MAX':
+            pool2d = nn.MaxPool2d
+        elif pool == 'AVG':
+            pool2d = nn.AvgPool2d
+        else:
+            raise Exception()
+        
+        self.maxpool_zg_p1 = pool2d(kernel_size=(12, 4))
+        self.maxpool_zg_p2 = pool2d(kernel_size=(24, 8))
+        self.maxpool_zg_p3 = pool2d(kernel_size=(24, 8))
+        self.maxpool_zp2 = pool2d(kernel_size=(12, 8))
+        self.maxpool_zp3 = pool2d(kernel_size=(8, 8))
 
-        self.maxpool_zg_p1 = nn.MaxPool2d(kernel_size=(12, 4))
-        self.maxpool_zg_p2 = nn.MaxPool2d(kernel_size=(24, 8))
-        self.maxpool_zg_p3 = nn.MaxPool2d(kernel_size=(24, 8))
-        self.maxpool_zp2 = nn.MaxPool2d(kernel_size=(12, 8))
-        self.maxpool_zp3 = nn.MaxPool2d(kernel_size=(8, 8))
-
-        self.reduction = nn.Sequential(nn.Conv2d(2048, self.feats, 1, bias=False), nn.BatchNorm2d(self.feats), nn.ReLU())
+        self.reduction = nn.Sequential(nn.Conv2d(2048, self.feats, 1, bias=False), nn.BatchNorm2d(self.feats_256), nn.ReLU())
 
         self._init_reduction(self.reduction)
 #         self.reduction.apply(_init_reduction)
 
-        self.fc_id_2048_0 = nn.Linear(self.n_feats, num_classes)
-        self.fc_id_2048_1 = nn.Linear(self.n_feats, num_classes)
-        self.fc_id_2048_2 = nn.Linear(self.n_feats, num_classes)
+        self.fc_id_2048_0 = nn.Linear(self.feats_2048, num_classes)
+        self.fc_id_2048_1 = nn.Linear(self.feats_2048, num_classes)
+        self.fc_id_2048_2 = nn.Linear(self.feats_2048, num_classes)
 
-        self.fc_id_256_1_0 = nn.Linear(self.feats, num_classes)
-        self.fc_id_256_1_1 = nn.Linear(self.feats, num_classes)
-        self.fc_id_256_2_0 = nn.Linear(self.feats, num_classes)
-        self.fc_id_256_2_1 = nn.Linear(self.feats, num_classes)
-        self.fc_id_256_2_2 = nn.Linear(self.feats, num_classes)
+        self.fc_id_256_1_0 = nn.Linear(self.feats_256, num_classes)
+        self.fc_id_256_1_1 = nn.Linear(self.feats_256, num_classes)
+        self.fc_id_256_2_0 = nn.Linear(self.feats_256, num_classes)
+        self.fc_id_256_2_1 = nn.Linear(self.feats_256, num_classes)
+        self.fc_id_256_2_2 = nn.Linear(self.feats_256, num_classes)
 
         self._init_fc(self.fc_id_2048_0)
         self._init_fc(self.fc_id_2048_1)
@@ -83,16 +76,6 @@ class MGN2(BasicModule):
         self._init_fc(self.fc_id_256_2_0)
         self._init_fc(self.fc_id_256_2_1)
         self._init_fc(self.fc_id_256_2_2)
-
-#         self.fc_id_2048_0.apply(_init_fc)
-#         self.fc_id_2048_1.apply(_init_fc)
-#         self.fc_id_2048_2.apply(_init_fc)
-
-#         self.fc_id_256_1_0.apply(_init_fc)
-#         self.fc_id_256_1_1.apply(_init_fc)
-#         self.fc_id_256_2_0.apply(_init_fc)
-#         self.fc_id_256_2_1.apply(_init_fc)
-#         self.fc_id_256_2_2.apply(_init_fc)
         
     @staticmethod
     def _init_reduction(reduction):
@@ -162,5 +145,5 @@ class MGN2(BasicModule):
             return cls_score, global_feat  # global feature for triplet loss
         else:
             #Concatenate all features and return
-            feat = torch.cat([fc_2048_0, fc_2048_1, f0_p2, f1_p2, fc_2048_2,  f0_p3, f1_p3, f2_p3], dim=1)
+            feat = torch.cat([fg_p1, fg_p2, fg_p3, f0_p2, f1_p2, f0_p3, f1_p3, f2_p3], dim=1)
             return feat
